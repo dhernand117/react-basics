1f84f5a64d2e5d400e17ef046515f3be2c79ca9e
Diego Hernandez <diegohervel@gmail.com>
2021-02-21 11:02:13 -0500

    Created the new repository for testing fullstack react app

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search/search-box.jsx"
+import './App.css';
+
+export default class App extends Component {
+  constructor(){
+    super();
+      this.state = {
+      monsters: [],
+      searchField: ''
+    };
+    //Way of how "this" have to be binded to the method if not using arrow functions
+   // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleChange =(e) => {
+      this.setState({ searchField: e.target.value });
+   }
+
+  render() {
+    //* This way of destructure is equal to use:
+    //* const monsters = this.state.monsters;
+    //* const searchField = this.state.searchField;
+    const { monsters, searchField } = this.state; 
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    
+    return (
+      <div className="App">
+        <SearchBox placeholder='search monsters'
+        handleChange={this.handleChange}  />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
